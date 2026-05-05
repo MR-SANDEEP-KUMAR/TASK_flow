690ff17262ed6f1fce950e99bce37d3026cdbd49
MR-SANDEE-KUMAR <snkumar2704@gmail.com>
2026-05-05 10:58:22 +0530

    FIED ERROR

--- Server/server.js
+++ Server/server.js
@@ -34,13 +34,14 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('✅ MongoDB connected');
+    console.log("MongoDB connected");
+
     app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.error('❌ DB connection error:', err.message);
-    process.exit(1);
+    console.error("DB error:", err);
   });
 
 module.exports = app;
