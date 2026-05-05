14a80a515799ca61c6970d1fc7f8788c0b2d7134
MR-SANDEE-KUMAR <snkumar2704@gmail.com>
2026-05-05 10:52:41 +0530

    port binding

--- Server/server.js
+++ Server/server.js
@@ -35,7 +35,8 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
   })
   .catch((err) => {
     console.error('❌ DB connection error:', err.message);
